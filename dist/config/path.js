d8dd5a9f2e2b98986fcad3010d272d658e811de6
wangchao <wangchao@youzan.com>
2017-06-06 18:14:28 +0800

    chore: build

---
+++ dist/config/path.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+    SAVEDIR: process.env.TRACE_DIR || '/tmp'
+};
---
+++ dist/lib/heap_snapshot.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const fs = require('fs');
+const profiler = require('v8-profiler');
+const SAVEDIR = require('../config/path').SAVEDIR;
+
+const snapshot = ctx => {
+    const name = `snapshot_${Date.now()}.heapsnapshot`;
+    const snapshot = profiler.takeSnapshot(name);
+    snapshot.export((error, result) => {
+        fs.writeFile(`${SAVEDIR}/${name}`, result, err => {
+            if (err) throw err;
+            snapshot.delete();
+        });
+    });
+
+    return {
+        filename: name,
+        msg: 'Please download snapshot file several seconds later'
+    };
+};
+
+module.exports = snapshot;
